cf86b92fe39a716d9278f950dae3c35ccbc12c42
Critchleyb <Critchley_11@hotmail.co.uk>
2020-07-13 13:47:03 +0100

    practice-app structure & components

--- practice-app/e2e/src/app.e2e-spec.ts
+++ practice-app/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('PracticeApp app is running!');
+    expect(page.getTitleText()).toEqual('practice-app app is running!');
   });
 
   afterEach(async () => {
--- practice-app/karma.conf.js
+++ practice-app/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/PracticeApp'),
+      dir: require('path').join(__dirname, './coverage/practice-app'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
