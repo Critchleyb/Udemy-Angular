920a2638a8d54ea10b4c7ae9c293cf94f09cf1e6
Critchleyb <Critchley_11@hotmail.co.uk>
2020-07-14 14:03:24 +0100

    Added Layout for Recipe Items

--- practice-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ practice-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe','This is a test','https://www.seekpng.com/png/full/1011-10115301_pizza-slices-clipart-pizza-stickers.png')
+  ];
 
   constructor() { }
 
