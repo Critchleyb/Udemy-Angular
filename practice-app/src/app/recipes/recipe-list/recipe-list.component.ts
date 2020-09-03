0cb338c6246ae5d6b2869673e972ba7c1724cb28
Critchleyb <Critchley_11@hotmail.co.uk>
2020-09-03 09:48:14 +0100

    End of section 16

--- practice-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ practice-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,28 +2,31 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RecipeService } from '../recipe.service';
 
 import { Recipe } from '../recipe.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
-export class RecipeListComponent implements OnInit, OnDestroy {
+export class RecipeListComponent implements OnInit ,OnDestroy 
+{
 
   recipes: Recipe[];
+  recipesChangedSubscription: Subscription;
 
 
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
-    this.recipeService.recipesChanged.subscribe((recipes: Recipe[]) => {
+    this.recipesChangedSubscription = this.recipeService.recipesChanged.subscribe((recipes: Recipe[]) => {
       this.recipes = recipes;
     });
   }
 
   ngOnDestroy() {
-    this.recipeService.recipesChanged.unsubscribe();
+    this.recipesChangedSubscription.unsubscribe();
   }
 
 }
--- practice-app/src/app/recipes/recipe.service.ts
+++ practice-app/src/app/recipes/recipe.service.ts
@@ -7,6 +7,7 @@ import { Subject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
   private recipes: Recipe[] = [
